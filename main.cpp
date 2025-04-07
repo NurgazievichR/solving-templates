c9593f7b74b4fdd908525ecbde52c55cd7b096c4
NurgazievichR <zalalidinovroma@gmail.com>
2025-04-07 21:42:12 +0600

    Disjoint Set Union

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 using ll = long long;
 #define all(x) x.begin(), x.end()
 
+int n;
 void solve() {
 
 }
@@ -23,3 +24,26 @@ int main() {
     return 0;
 }
 
+int maxn, MOD;
+
+//DSU (Disjoint Set Union)
+//Система непересекающихся множеств
+
+//parent хранит представителя множества (корень), size хранит размер компоненты связности только для корней
+int parent[maxn], size_dsu[maxn];
+
+int find_dsu(int x) {
+    if (parent[x]!=x) parent[x] = find_dsu(parent[x]);
+    return parent[x];
+}
+
+void union_dsu(int a, int b) {
+    int a_parent = find_dsu(a);
+    int b_parent = find_dsu(b);
+
+    if (size_dsu[a_parent] < size_dsu[b_parent]) {
+        swap(a_parent, b_parent);
+    }
+    parent[b_parent] = a_parent;
+    size_dsu[a_parent] += size_dsu[b_parent];
+}
