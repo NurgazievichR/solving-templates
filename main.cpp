206cc5cc5819a538100b9c4c4b961fb05a4aad36
NurgazievichR <zalalidinovroma@gmail.com>
2025-04-07 21:32:03 +0600

    C++ base template

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//C++ base template
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+#define all(x) x.begin(), x.end()
+
+void solve() {
+
+}
+
+void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+}
+
+int tt = 1;
+int main() {
+    fast_io();
+    cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
+
